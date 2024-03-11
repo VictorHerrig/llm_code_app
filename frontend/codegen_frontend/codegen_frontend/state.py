04abcda4cfd07a45508289f12b1e089d84f56f4d
VictorHerrig <github.mhms0@simplelogin.com>
2024-03-11 11:13:37 -0700

    init frontend with reflex

---
+++ frontend/codegen_frontend/codegen_frontend/state.py
@@ -0,0 +1,148 @@
+from uuid import uuid4
+
+import asyncio
+
+import reflex as rx
+
+
+def is_error(answer: str) -> bool:
+    return answer is None or len(answer) < 5 or answer.startswith("ERROR")
+
+
+class State(rx.State):
+    query: str = None
+    model_name: str = None
+    lang_dict: dict[str, str] = {
+        "Python": "python",
+        "C++": "cpp",
+        "C": "c",
+        "SQL": "sql",
+        "Java": "java",
+        "Scala": "scala",
+        "bash": "bash",
+        "Javascript": "javascript",
+        "Typescript": "typescript",
+    }
+    model_dict: dict[str, str] = {
+        'GPT-3.5': 'gpt-3.5-turbo-0125',
+        'Hermes': 'teknium/OpenHermes-2.5-Mistral-7B'
+    }
+    inv_lang_dict: dict[str, str] = {v: k for k, v in lang_dict.items()}
+    inv_model_dict: dict[str, str] = {v: k for k, v in model_dict.items()}
+    language: str = None
+
+    active_session: tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]] = ()
+
+    session_1_id = str(uuid4())
+    session_2_id = str(uuid4())
+    inactive_sessions: list[
+        tuple[str, tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]]]
+    ] = []
+
+    errors: list[str] = []
+    new_session_id: str = ''
+    _loading: bool = False
+
+    @rx.var
+    def active_session_id(self) -> str:
+        if self.has_active_session:
+            return self.active_session[0]
+        return ''
+
+    @rx.var
+    def is_loading(self) -> bool:
+        return self._loading
+
+    @rx.var
+    def session_language(self) -> str:
+        if self.has_active_session:
+            return self.active_session[1]
+        return ''
+
+    @rx.var
+    def session_model(self) -> str:
+        if self.has_active_session:
+            return self.active_session[2]
+        return ''
+
+    @rx.var
+    def session_history(self) -> list[tuple[str, str, str, str, str, str, bool]]:
+        if self.has_active_session:
+            return self.active_session[3]
+        return []
+
+    @rx.var
+    def has_active_session(self) -> bool:
+        return self.active_session is not None and len(self.active_session) > 0
+
+    @rx.var
+    def has_inactive_sessions(self) -> bool:
+        return self.inactive_sessions is not None and len(self.inactive_sessions) > 0 and len(self.inactive_sessions[0]) == 2 and len(self.inactive_sessions[0][1]) > 0
+
+    @rx.var
+    def inactive_session_history(
+        self,
+    ) -> list[tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]]]:
+        if self.has_inactive_sessions:
+            return [s[1] for s in self.inactive_sessions]
+        else:
+            return []
+
+    async def call_backend(self):
+        self._loading = True
+        self.errors = []
+        if self.query is None or len(self.query) < 5:
+            self.errors.append("Please input a prompt of at least 5 character")
+        if self.query is not None and len(self.query) > 200:
+            self.errors.append("Please limit input a prompt length to 200 character")
+        if len(self.errors) == 0:
+            await asyncio.sleep(1.)
+            answer = "ERROR: No more code left :("
+
+            turn = (
+                self.active_session[0],  # session_id
+                str(uuid4()),            # turn_id
+                self.active_session[1],  # language_id
+                self.active_session[2],  # model_id
+                self.query,              # query
+                answer,                  # answer
+                is_error(answer),        # answer_is_error
+            )
+            self.active_session[3].insert(0, turn)
+        self._loading = False
+
+    def new_session(self):
+        self.errors = []
+        if self.language is None or len(self.language) == 0:
+            self.errors.append("Please select a language")
+        if self.model_name is None or len(self.model_name) == 0:
+            self.errors.append("Please select a model")
+        if len(self.errors) == 0:
+            if self.has_active_session and len(self.active_session[3]) > 0:
+                self.inactive_sessions.append((self.active_session[0], self.active_session))
+
+            session_id = str(uuid4())
+            self.active_session = (session_id, self.lang_dict[self.language], self.model_dict[self.model_name], [])
+
+    def change_session(self):
+        if self.new_session_id is not None and any(
+            [i == self.new_session_id for i, _ in self.inactive_sessions]
+        ):
+            idx = [
+                j
+                for j, (i, _) in enumerate(self.inactive_sessions)
+                if i == self.new_session_id
+            ][0]
+            new_activate_session = self.inactive_sessions[idx][1]
+            self.inactive_sessions.pop(idx)
+
+            if self.has_active_session and len(self.active_session[1]) > 0:
+                inactive_session_id = self.active_session[0]
+                self.inactive_sessions.append(
+                    (inactive_session_id, self.active_session)
+                )
+
+            self.active_session = new_activate_session
+            self.language = self.active_session[0]
+
+            self.new_session_id = None
---
+++ frontend/codegen_frontend/rxconfig.py
@@ -0,0 +1,8 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="codegen_frontend",
+    backend_port=8082,
+    #api_url='localhost',
+    frontend_port=8084,
+)
