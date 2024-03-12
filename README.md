7bb085cc7dce7cf54087aab9f2cb8c8e6a82b8d8
VictorHerrig <github.mhms0@simplelogin.com>
2024-03-11 20:06:27 -0700

    Wrote readme, cleaned up code and UI elements, added comments, fixed a few little bugs

--- README.md
+++ README.md
@@ -1,31 +1,160 @@
 # Code generation app
-## Docker compose
-### Building and running the images
-From the llm_code_app directory, simply run
+
+## Quickstart
+
+Ensure you have the GPU container toolkit if you want to use a local LLM (see Docker
+compose section).
+
+From the llm_code_app directory run:
+
 ```shell
 docker compose up
 ```
+
+Wait for the containers to build and run. This can take a few minutes. The app will
+be running at `localhost:8084`
+
+## About the repository
+
+Normally, I would squash, merge then delete feature branches. I have not done that to
+allow looking further into each commit if so desired.
+
+## Backend features
+
+Uses FastAPI with two post endpoints: generate and adjust. The first is used on the
+first 'turn.' The second is used when the user has already generated some code and is
+providing feedback to adjust it.
+
+### Feedback Loop
+
+The adjust endpoint is the backend portion of this essential component.
+
+### Custom LLM integration
+
+I have already explicitly added a custom LLM: Hermes. The LLMBackend class structure is
+very straightforward, so adding further models should be rather simple. For models
+following the Huggingface structure, you only have so subclass the `HuggingfaceBackend`
+class and implement the `encode_prompt` and `decode_output` methods. These are used to
+format and tokenize the input prompt and decode outputs based on model-specific needs.
+
+If you instead want to include a model with a different structure, you will need to
+implement the base `LLMBackend` class.
+
+### Containerization
+
+The backend is packaged in a docker container and is only visible to the frontend
+container.
+
+### Prompt Security
+
+Utilizes specific system prompts and prompt layout, along with low temperature to
+ensure stable generation. Additionally, error returns are flagged. However, there is
+always the possibility something gets through, and in that case further measures, like
+doing a double query (Is this code?) would be advisable, as well as more thorough
+pre-processing to check for attacks.
+
+## Frontend features
+
+I am not a frontend engineer! But I can work with it to some extent. Here I've used the
+Reflex framework in python, which basically just compiles python into javascript.
+
+### Web interface
+
+A core component. The frontend does this well enough. I have created 'sessions' that
+users can create and swap between, each with their own language and model. This keeps
+some ambiguity from creeping in (switching between models and languages after some
+generations) and makes it easier to keep track of.
+
+### Feedback loop
+
+The feedback loop happens automatically, allowing users to send multiple messages, with
+each message after the first being feedback on the previously generated code snippet.
+
+### Custom LLM integration
+
+Most of this work happens on the backend, but the frontend provides a selector for model
+name, which is then converted to the model id and sent to the backend API.
+
+### Containerization
+
+As with the backend, containerized. Reflex uses a different project structure and python
+version, meaning splitting the frontend and backend into separate containers has
+multiple motivations. It communicates with the backend via the default docker compose
+network, but since the browser needs to communicate with the Reflex "backend," the ports
+for both the frontend and Reflex "backend" are forwarded through.
+
+### Prompt Security
+
+Simple checks for very long or short prompts, and boxing the model and language choices
+also helps. Errors are clearly marked.
+
+### Snippet Management
+
+Part of this is implemented in the session system, namely creating new sessions along
+with looking at and switching to old sessions. Snippet and session deletion aren't
+implemented, but they are a fairly straightforward extrapolation.
+
+### Snippet Testing System
+
+Not implemented. I know there is an OpenAI endpoint for this, I just didn't wish to use
+it for this project. Local interpretation could also be used, but is riskier.
+
+### A note about data structures
+
+Reflex supports Pydantic data models and dicts, or so they say. I simply could not get
+even the simplest to compile in this project, so I had to revert to using primitives,
+lists and tuples. My apologies, I tried to make it as readable as possible.
+
+## Docker compose
+
+### Building and running the images
+
+From the llm_code_app directory, simply run
+
+```shell
+docker compose up
+```
+
 That's it. GPU, and by extension the Huggingface models, will not be available unless
 you follow the directions below.
 
-Building the backend docker will take some time as the torch and nvidia packages are
-quite large and take a while to download.
+Building the backend docker image will take some time as the torch and nvidia packages
+are quite large and take a while to download. The frontend docker also needs to compile
+and export the python into javascript, which can take a few minutes.
 
 ### Using GPU inside the compose
-The provided compose file will only work if GPU containers are enables. This will
+
+The provided compose file will only work if GPU containers are enabled. This will
 require some work on your part, if you haven't done it already. If you are running
 Windows, there is a built-in container-level GPU functionaliy. I don't use Windows, but
 you can read more here: `https://docs.docker.com/desktop/gpu/`. I can't give any
 guarantees on this working for Windows.
 
-Now, for Unix systems, the process can happen on the compose level. First, you will need
-to install the Nvidia container toolkit and configure the docker daemon accordingly:
-`https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/
-latest/install-guide.html`.
+Now, for Unix systems, you will need to install the Nvidia container toolkit and
+configure the docker daemon accordingly: `https://docs.nvidia.com/datacenter/
+cloud-native/container-toolkit/latest/install-guide.html`.
 
-And the rest is done for you in the `compose.yaml`.
+And the rest is done for you in the `compose.yaml`. Only the backend can access the gpu.
 
 ### Volumes
+
 I haven't used any volumes in any of the docker files. If you add in volumes, the
 overhead for loading models for the first time could be significantly reduced (esp. wrt.
 downloading from the hub).
+
+## Known Issues
+
+I am lazily instantiating models in the backed, meaning large models may need to be
+downloaded and will need to be loaded into memory after the API call. This will time out
+the call on the frontend, which means you'll just have to repeat the call when the model
+has finished loading. May need to refresh the page, too.
+
+Language and model dropdowns sometimes save state across reloads separate the underlying
+variable state. This results in erroneous error boxes. Just change the value and
+change it back in each box.
+
+Adding a new session or switching to an old session when the active session is blank
+can result in fatal errors. Clear website data and reload.
+
+Sometimes the frontend loses connection to the Reflex "backend" for no apparent reason.
+Restarting the docker fixes this.
--- frontend/codegen_frontend/codegen_frontend/state.py
+++ frontend/codegen_frontend/codegen_frontend/state.py
@@ -1,10 +1,9 @@
 import json
 from uuid import uuid4
 
+import reflex as rx
 import requests
 from requests.exceptions import HTTPError
-
-import reflex as rx
 
 
 def is_error(answer: str) -> bool:
@@ -12,8 +11,22 @@ def is_error(answer: str) -> bool:
 
 
 class State(rx.State):
+    # ------------------------------- #
+    # --------- State vars ---------- #
+    # ------------------------------- #
+
     query: str = None
     model_name: str = None
+    language: str = None
+    new_session_id: str = ""
+    _loading: bool = False
+
+    # ------------------------------- #
+    # --------- Backend vars -------- #
+    # ------------------------------- #
+
+    _backend_generate_url: str = "http://backend:80/generate"
+    _backend_adjust_url: str = "http://backend:80/adjust"
     lang_dict: dict[str, str] = {
         "Python": "python",
         "C++": "cpp",
@@ -25,36 +38,44 @@ class State(rx.State):
         "bash": "bash",
     }
     model_dict: dict[str, str] = {
-        'GPT-3.5': 'gpt-3.5-turbo-0125',
-        'Hermes': 'teknium/OpenHermes-2.5-Mistral-7B'
+        "GPT-3.5": "gpt-3.5-turbo-0125",
+        "Hermes": "teknium/OpenHermes-2.5-Mistral-7B",
     }
+    backend_lang_dict: dict[str, str] = {"cpp": "c++"}
     inv_lang_dict: dict[str, str] = {v: k for k, v in lang_dict.items()}
     inv_model_dict: dict[str, str] = {v: k for k, v in model_dict.items()}
-    backend_lang_dict: dict[str, str] = {
-        "cpp": "c++"
-    }
+    _max_char: int = 1000
 
-    language: str = None
+    # ------------------------------- #
+    # ----- Data structure vars ----- #
+    # ------------------------------- #
 
-    active_session: tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]] = ()
-
-    session_1_id = str(uuid4())
-    session_2_id = str(uuid4())
+    active_session: tuple[
+        str, str, str, list[tuple[str, str, str, str, str, str, bool]]
+    ] = ()
     inactive_sessions: list[
-        tuple[str, tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]]]
+        tuple[
+            str, tuple[str, str, str, list[tuple[str, str, str, str, str, str, bool]]]
+        ]
     ] = []
-
     errors: list[str] = []
-    new_session_id: str = ''
-    _loading: bool = False
-    _backend_generate_url: str = "http://backend:80/generate"
-    _backend_adjust_url: str = "http://backend:80/adjust"
+
+    # ------------------------------- #
+    # -------- Computed vars -------- #
+    # ------------------------------- #
+
+    @rx.var
+    def call_type(self) -> str:
+        if not self.has_active_session or len(self.active_session[3]) == 0:
+            return "Generate a new code snippet to do something"
+        else:
+            return "Add feedback to adjust the last code snippet"
 
     @rx.var
     def active_session_id(self) -> str:
         if self.has_active_session:
             return self.active_session[0]
-        return ''
+        return ""
 
     @rx.var
     def is_loading(self) -> bool:
@@ -64,13 +85,13 @@ class State(rx.State):
     def session_language(self) -> str:
         if self.has_active_session:
             return self.active_session[1]
-        return ''
+        return ""
 
     @rx.var
     def session_model(self) -> str:
         if self.has_active_session:
             return self.active_session[2]
-        return ''
+        return ""
 
     @rx.var
     def session_history(self) -> list[tuple[str, str, str, str, str, str, bool]]:
@@ -84,7 +105,12 @@ class State(rx.State):
 
     @rx.var
     def has_inactive_sessions(self) -> bool:
-        return self.inactive_sessions is not None and len(self.inactive_sessions) > 0 and len(self.inactive_sessions[0]) == 2 and len(self.inactive_sessions[0][1]) > 0
+        return (
+            self.inactive_sessions is not None
+            and len(self.inactive_sessions) > 0
+            and len(self.inactive_sessions[0]) == 2
+            and len(self.inactive_sessions[0][1]) > 0
+        )
 
     @rx.var
     def inactive_session_history(
@@ -95,7 +121,12 @@ class State(rx.State):
         else:
             return []
 
-    def api_call(self):
+    # ------------------------------- #
+    # -------- Event handlers ------- #
+    # ------------------------------- #
+
+    def _api_call(self):
+        """Call either the generate or adjust endpoint depending on the current state"""
         # Fetch language and model name in backend-readable format
         language = self.active_session[1]
         model_name = self.active_session[2]
@@ -105,13 +136,11 @@ class State(rx.State):
         if len(self.active_session[3]) == 0:
             # Generation
             payload = dict(
-                model_name=model_name,
-                language=language,
-                original_prompt=self.query
+                model_name=model_name, language=language, original_prompt=self.query
             )
             response = requests.post(self._backend_generate_url, json=payload)
             if response.status_code == 200:
-                return json.loads(response.content.decode('utf-8'))['code_out']
+                return json.loads(response.content.decode("utf-8"))["code_out"]
             else:
                 raise HTTPError(f"{response.status_code}: {response.content}")
         else:
@@ -123,39 +152,45 @@ class State(rx.State):
                 language=language,
                 original_prompt=first_query,
                 reference_code=last_answer,
-                feedback_prompt=self.query
+                feedback_prompt=self.query,
             )
             response = requests.post(self._backend_adjust_url, json=payload)
             if response.status_code == 200:
-                return json.loads(response.content.decode('utf-8'))['code_out']
+                return json.loads(response.content.decode("utf-8"))["code_out"]
             else:
                 raise HTTPError(f"{response.status_code}: {response.content}")
 
     def submit(self):
+        """Submit the text to the backend if there are no errors."""
         self.errors = []
         if self.query is None or len(self.query) < 5:
             self.errors.append("Please input a prompt of at least 5 character")
-        if self.query is not None and len(self.query) > 200:
-            self.errors.append("Please limit input a prompt length to 200 character")
+        if self.query is not None and len(self.query) > self._max_char:
+            self.errors.append(
+                f"Please limit input a prompt length to {self._max_char} characters"
+            )
         if len(self.errors) == 0:
+            self._loading = True
             try:
-                answer = self.api_call()
+                answer = self._api_call()
                 if answer is None:
                     self.errors.append("Backend error")
                 turn = (
                     self.active_session[0],  # session_id
-                    str(uuid4()),            # turn_id
+                    str(uuid4()),  # turn_id
                     self.active_session[1],  # language_id
                     self.active_session[2],  # model_id
-                    self.query,              # query
-                    answer,                  # answer
-                    is_error(answer),        # answer_is_error
+                    self.query,  # query
+                    answer,  # answer
+                    is_error(answer),  # answer_is_error
                 )
                 self.active_session[3].insert(0, turn)
             except HTTPError as e:
                 self.errors.append(f"Backend error: {str(e)}")
+            self._loading = False
 
     def new_session(self):
+        """Create a new session and make the current one inactive."""
         self.errors = []
         if self.language is None or len(self.language) == 0:
             self.errors.append("Please select a language")
@@ -163,12 +198,20 @@ class State(rx.State):
             self.errors.append("Please select a model")
         if len(self.errors) == 0:
             if self.has_active_session and len(self.active_session[3]) > 0:
-                self.inactive_sessions.append((self.active_session[0], self.active_session))
+                self.inactive_sessions.append(
+                    (self.active_session[0], self.active_session)
+                )
 
             session_id = str(uuid4())
-            self.active_session = (session_id, self.lang_dict[self.language], self.model_dict[self.model_name], [])
+            self.active_session = (
+                session_id,
+                self.lang_dict[self.language],
+                self.model_dict[self.model_name],
+                [],
+            )
 
     def change_session(self):
+        """Change the active session to the selected inactive session."""
         if self.new_session_id is not None and any(
             [i == self.new_session_id for i, _ in self.inactive_sessions]
         ):
